9e1cf476d4884a3146963f00651d87cfcfa4fa6f
Uvindu Shehan Perera <44149708+uvinduperera@users.noreply.github.com>
2020-12-25 20:24:27 +0530

    Update rank.sh

--- rank.sh
+++ rank.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+
+function noValidate{
+
+
+if [ -z "$1" ]
+
+	then
+
+	  echo 'Inputs cannot be blank please try again'
+
+	  exit 0
+
+	fi
+
+	 
+
+	#Now Validate if the user input is a number (Integer or Float)
+
+	#If an input field not a number, display appropriate message and stop execution of script
+
+	if ! [[ "$1" =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]
+	    then
+
+	        echo "Inputs must be a numbers"
+
+	    exit 0
+
+	fi
+}
+
+
+
 #Welcome to bash ranking system by satriaana
 echo 'Welcome to Satriaana Ranking System. Please provide the following information to calculate the rankings of the Teams!'
 #Asking for weekly cordinator name
@@ -15,30 +47,7 @@ read -p 'Can I know the weekly milestone? :'milestone
 #ask for total no of issues in blog
 
 read -p 'How many issues can you see in the $milestone milestone? :'totalIssues
-if [ -z "$totalIssues" ]
 
-	then
-
-	  echo 'Inputs cannot be blank please try again'
-
-	  exit 0
-
-	fi
-
-	 
-#Now Validate if the user input is a number (Integer or Float)
-
-#If an input field not a number, display appropriate message and stop execution of script
-
-	if ! [[ "$totalIssues" =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]
-	    then
-
-	        echo "Inputs must be a numbers"
-
-	    exit 0
-
-	fi
-  
 #an issue responded within three days in blog 
 
 
