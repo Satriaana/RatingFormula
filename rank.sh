049bbe6c8913ef728e572b5321f610265e0f5710
Uvindu Shehan Perera <44149708+uvinduperera@users.noreply.github.com>
2020-12-25 18:44:16 +0530

    Update rank.sh

--- rank.sh
+++ rank.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+#Welcome to bash ranking system by satriaana
+
+#Asking for weekly cordinator name
+
+#welcome by name
+
+#week start date
+
+
+#check blog team milestone creation (1,0)
+
+#ask for total no of issues in blog
+
+#an issue responded within three days in blog 
+
+#ask for responded no of issues in blog
+
+#ask for total complted tasks in blog
+
